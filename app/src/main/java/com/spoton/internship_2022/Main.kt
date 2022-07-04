d682922a1697a16775f8e6434b09bfac15890232
Karolina Kotlowska <108656249+karolakotlowska@users.noreply.github.com>
2022-07-04 13:32:43 +0200

    my second message (#5)

--- app/src/main/java/com/spoton/internship_2022/Main.kt
+++ app/src/main/java/com/spoton/internship_2022/Main.kt
@@ -2,4 +2,7 @@ package com.spoton.internship_2022
 
 fun main(args: Array<String>) {
     println("Hello world!")
+    println("Hello world!")
+    println("Hello world!")
+    println("Hello world!")
 }
